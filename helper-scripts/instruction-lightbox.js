e62e0ffe42fda5fb262ab0c9c14bee475d03a8fe
titian <titianchan@hotmail.com>
2016-12-13 03:21:40 +0000

    turning in

--- helper-scripts/instruction-lightbox.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $('.lightbox').magnificPopup({
-    type:'image',
-    gallery: {enabled:true},
-    titleSrc: function(item) {
-      return item.el.attr('title');
-    }
-  });
-});
